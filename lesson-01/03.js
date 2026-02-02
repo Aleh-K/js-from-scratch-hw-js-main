16214ae4fd2ee15bf1a10055d8e7876b6f715637
Aleh Kurnevich <Leonchi_c@mail.ru>
2026-02-02 13:57:38 +0300

    compl task 1,2

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -5,4 +5,12 @@
 */
 
 // your code
-let sum
+let sum = 0;
+let num = 1;
+while (num <= 20) {
+    if ((num % 2) === 1) {
+        sum = sum + num;
+    } else {}
+    num += 1;
+}
+console.log(sum);
